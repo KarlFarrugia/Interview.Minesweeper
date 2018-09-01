1bd5909f64d3ff60d8b1bd7f7a4d03cf8f590ff6
Karl Farrugia <karlfarrugia@live.com>
2018-09-01 05:12:02 +0200

    Added some tests Finished comments Polished some code in the program

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Minesweeper.Game;
+using Minesweeper.Game.BoardComponents;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// A set of Unit Tests with the aim of testing different aspects of the minesweeper program execution.
+    /// </summary>
+    [TestFixture]
+    public class Tests
+    {
+        /// <summary>
+        /// Tests that the <see cref="Coordinates"/> structure holds the X and Y coordinates properly.
+        /// </summary>
+        [TestCase]
+        public void TestCoordinates()
+        {
+            var coordinates = new Coordinates {CoordinateX = 4, CoordinateY = 2};
+            Assert.AreEqual(coordinates.CoordinateX, 4);
+            Assert.AreEqual(coordinates.CoordinateY, 2);
+        }
+        
+        /// <summary>
+        /// Tests that an <see cref="Mine"/> cannot be null.
+        /// </summary>
+        [TestCase]
+        public void TestNullMine()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Mine()); 
+        }
+        
+        /// <summary>
+        /// Tests that an <see cref="Safe"/> cannot be null.
+        /// </summary>
+        [TestCase]
+        public void TestNullSafe()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Safe()); 
+        }
+    }
+}
